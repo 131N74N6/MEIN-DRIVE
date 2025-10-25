f3485d82cbd48963cea7bab73d3317904fe34964
unknown <bintangkusumam@gmail.com>
2025-10-25 21:48:59 +0700

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
+const queryClient = new QueryClient();
+
 export default function App() {
     return (
-        <div>App</div>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/sign-in' element={<SignIn/>}/>
+                    <Route path='/sign-up' element={<SignUp/>}/>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     )
 }
--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,5 +1,14 @@
 export default function SignIn() {
     return (
-        <div>SignIn</div>
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex">
+                <div className="bg-red-700 text-white font-[500] p-[1rem] w-[200px] h-[400px]"></div>
+                <form className="bg-white shadow-[0_0_4px_#1a1a1a] p-[1rem] w-[200px] h-[400px] flex flex-col gap-[1.3rem]">
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit" className="bg-pink-800 text-white font-[500] text-[0.9rem] p-[0.4rem]">Sign In</button>
+                </form>
+            </div>
+        </div>
     )
 }
