fac57ce0147255ea946dadca61ca87c10f6094f7
unknown <bintangkusumam@gmail.com>
2025-11-03 20:54:51 +0700

    new

--- api/src/routes/user.router.ts
+++ api/src/routes/user.router.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { getUserData, signIn, signUp } from "../controllers/user.controller";
+import { verifyToken } from "../middleware/auth.middleware";
 
 const userRoutes = Router();
 
 userRoutes.post('/sign-in', signIn);
-
 userRoutes.post('/sign-up', signUp);
-
-userRoutes.get('/user-data/:id', getUserData);
+userRoutes.get('/user-data/:id', verifyToken, getUserData);
 
 export default userRoutes;
